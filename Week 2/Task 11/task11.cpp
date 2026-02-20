0cd9f47f4971b45c54eb6d54f8acd9d768b058ca
Muhammad Nouman <gb9398610@Gmail.com>
2026-02-21 02:37:38 +0500

    2 Week task done

---
+++ Week 2/Task 11/task11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"         *         *        *                                        "<<endl;
+cout<<"          *         *        *                                       "<<endl;
+cout<<"           *         *        *                                      "<<endl;
+cout<<"          *        *         *                                       "<<endl;
+cout<<"         *        *         *                                        "<<endl;
+cout<<"          *        *         *                                       "<<endl;
+cout<<"           *         *        *                                      "<<endl;
+cout<<"  **************************************                             "<<endl;
+cout<<"   *                                  *                              "<<endl;
+cout<<"    *                               *                                "<<endl;
+cout<<"     *******************************                                 "<<endl;
+cout<<"      * 8   8   8   8   8   8   8 *                                  "<<endl;
+cout<<"        *************************                                    "<<endl<<endl;
+cout<<"      Wellcome To Foody Application                                  ";
+}
---
+++ Week 2/Task 8/task8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+
+system("color C7");
+cout<<"  .....                  eeeee    eeeee            ...........                 "<<endl;
+cout<<"  ......                e     e  e     e           ..........                  "<<endl;
+cout<<"  .......                  eee   e     e           .........                   "<<endl;
+cout<<"   .......               ee      e     e           ........                    "<<endl;
+cout<<"    .......             eeeeeee   eeeee  th        .......                     "<<endl;
+cout<<"     .......        ............................   ......                      "<<endl;
+cout<<"       ......             C E N T U R Y            .....                       "<<endl;
+cout<<"         .....      ............................   ....                        "<<endl;
+cout<<"           ...         eeeee eeeee e   e eeeee     ...                         "<<endl;
+cout<<"              --         e   e      e e    e       --                          "<<endl;
+cout<<"             _||_        e   eeee    ee    e      _||_                         "<<endl;
+cout<<"            |    |       e   e      e  e   e     |    |                        "<<endl;
+cout<<"      ______|____|_____  e   eeeee e    e  e ____|    |________                ";
+}
